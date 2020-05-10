7a691e1539b4a2dcce4f4e6b9adcfc49eb271ca8
Oyewole ABAYOMI S <oyewoleabayomi@gmail.com>
2020-05-10 22:01:11 +0100

    add event

--- src/EventManager.class.ts
+++ src/EventManager.class.ts
@@ -1,27 +1,32 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable array-callback-return */
 import { camelCase } from 'lodash';
+import { EventEmitter2 as EventEmitter } from 'eventemitter2';
 
-import emittery from './lib/event';
 import { IEventConfig } from './lib/interfaces';
 import AzureServiceBus from './azure/AzureServiceBus';
 
 export default class EventManager {
   private static instance: EventManager;
 
-  private serviceBus: AzureServiceBus | undefined;
+  private _emittery!: EventEmitter;
+
+  private serviceBus!: AzureServiceBus;
 
   private constructor() {}
 
   static getInstance(): EventManager {
     if (!EventManager.instance) {
       EventManager.instance = new EventManager();
+
+      EventManager.instance._emittery = new EventEmitter();
     }
+
     return EventManager.instance;
   }
 
   public initialize(config: IEventConfig) {
-    this.serviceBus = new AzureServiceBus(config, emittery);
+    this.serviceBus = new AzureServiceBus(config, this._emittery);
 
     config.subscription.map((subscriptionName, index) => {
       this.serviceBus?.receiver(subscriptionName);
@@ -30,8 +35,8 @@ export default class EventManager {
     });
   }
 
-  public static on(eventName, listener) {
-    emittery.addListener(camelCase(eventName), listener);
+  public on(eventName, listener) {
+    this._emittery.addListener(camelCase(eventName), listener);
   }
 
   public emit(eventNames: string | string[], payload: any) {
